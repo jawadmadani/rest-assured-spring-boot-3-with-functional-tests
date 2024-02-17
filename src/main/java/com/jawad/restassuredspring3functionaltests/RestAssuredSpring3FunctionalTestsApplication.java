d2a23da443929b42dfe3032d9e61b78fc499ac28
Jawad Madani <jawad.k.madani@gmail.com>
2024-02-17 14:26:46 +0000

    Creating spring boot 3 project with java 21 to test rest-assured for funcational tests

---
+++ src/main/java/com/jawad/restassuredspring3functionaltests/RestAssuredSpring3FunctionalTestsApplication.java
@@ -0,0 +1,13 @@
+package com.jawad.restassuredspring3functionaltests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAssuredSpring3FunctionalTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestAssuredSpring3FunctionalTestsApplication.class, args);
+    }
+
+}
